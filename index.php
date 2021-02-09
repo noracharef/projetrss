57379eb074d7efbb6ef863fb56d2919ed0723671
noracharef <nora6272@hotmail.fr>
2021-02-09 12:13:45 +0100

    modale

--- index.php
+++ index.php
@@ -12,9 +12,9 @@
 </head>
 
 <body>
-<?php include("controllers/index_controller.php");?> 
-   <!-- Navbar avec le bouton Parametres -->
-   <nav class="navbar navbar-light bg-light">
+  <?php include("controllers/index_controller.php"); ?>
+  <!-- Navbar avec le bouton Parametres -->
+  <nav class="navbar navbar-light bg-light">
     <div class="container-fluid">
       <a class="navbar-brand" data-bs-toggle="modal" data-bs-target="#modalParameters">
         <i class="fas fa-cog"></i>
@@ -23,50 +23,49 @@
   </nav>
   <!-- Modal affichant les paramètres -->
   <div class="modal fade" id="modalParameters" tabindex="-1" aria-labelledby="modalParametersLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Paramètres de personnalisation</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        <form method="post">
-          <div> 
-            <h5>Choix du thème graphique</h5>
-            <input type="radio" class="form-check-input" name="theme" id="theme1" value="theme1" checked><label for="theme1">Thème 1</label><br>
-            <input type="radio" class="form-check-input" name="theme" id="theme2" value="theme2"><label for="theme1">Thème 2</label><br>
-            <input type="radio" class="form-check-input" name="theme" id="theme3" value="theme3"><label for="theme1">Thème 3</label><br>
-          </div>
-          
-          <div>
-            <h5>Choix des flux RSS</h5><!-- Creer un input check pour chaque lien dans le tableau Feeds -->
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Paramètres de personnalisation</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form method="post">
             <div>
-            <?php foreach ($feeds as $key => $value)
-            {?>
-              <input type="checkbox" class="form-check-input" id="<?= $key ?>" name="<?= $key ?>" value="<?= $value ?>"><label for="<?= $key ?>"> <?= $key ?></label><br>
-              <?php }?>
+              <h5>Choix du thème graphique</h5>
+              <input type="radio" class="form-check-input" name="theme" id="theme1" value="theme1" checked><label for="theme1">Thème 1</label><br>
+              <input type="radio" class="form-check-input" name="theme" id="theme2" value="theme2"><label for="theme1">Thème 2</label><br>
+              <input type="radio" class="form-check-input" name="theme" id="theme3" value="theme3"><label for="theme1">Thème 3</label><br>
             </div>
-          </div>
 
-          <div>
-            <h5>Nombre d'articles affichés</h5>
-            <input type="radio" class="form-check-input" name="nbArticle" id="article5" value="5" checked><label for="article5">5</label><br>
-            <input type="radio" class="form-check-input" name="nbArticle" id="article10" value="10" checked><label for="article10">10</label><br>
-            <input type="radio" class="form-check-input" name="nbArticle" id="articleAll" value="All" checked><label for="articleAll">Tous les articles</label><br>
-          </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="submit" class="btn btn-primary" href="">Save changes</button>
+            <div>
+              <h5>Choix des flux RSS</h5><!-- Creer un input check pour chaque lien dans le tableau Feeds -->
+              <div>
+                <?php foreach ($feeds as $key => $value) { ?>
+                  <input type="checkbox" class="form-check-input" id="<?= $key ?>" name="<?= $key ?>" value="<?= $value ?>"><label for="<?= $key ?>"> <?= $key ?></label><br>
+                <?php } ?>
+              </div>
+            </div>
+
+            <div>
+              <h5>Nombre d'articles affichés</h5>
+              <input type="radio" class="form-check-input" name="nbArticle" id="article5" value="5" checked><label for="article5">5</label><br>
+              <input type="radio" class="form-check-input" name="nbArticle" id="article10" value="10" checked><label for="article10">10</label><br>
+              <input type="radio" class="form-check-input" name="nbArticle" id="articleAll" value="All" checked><label for="articleAll">Tous les articles</label><br>
+            </div>
+          </form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="submit" class="btn btn-primary" href="">Save changes</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
 
 
 
-<div class="row justify-content-center">
+  <div class="row justify-content-center">
     <!-- Creer une col + card à chaque article du tableau Posts -->
     <div class="col-9 col-md-3 border m-2">
       <h1 style="text-align:center">test</h1>
@@ -113,7 +112,7 @@
         <i class="fas fa-plus mx-1"></i>Plus d'infos
       </button>
     </div>
-    
+
   </div>
 
 
