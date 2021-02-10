f0ef9b8966827ecd53c299b6c80705fddcf80097
CharefSarah <charef.sarah@gmx.fr>
2021-02-10 14:42:34 +0100

    ajout des themes

--- index.php
+++ index.php
@@ -11,40 +11,75 @@
   <title>Document</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
-  <link rel="stylesheet" href="assets/css/style2.css" />
+ 
+ <?php if ($_COOKIE["theme"] == 1 || ! isset($_COOKIE["theme"])){ ?>
+    <link rel="stylesheet" href="assets/css/style2.css" />
+<?php } elseif ($_COOKIE["theme"] == 2 ){ ?>
+<link rel="stylesheet" href="assets/css/style2.css" />
+<?php } elseif ($_COOKIE["theme"] == 3 ){ ?>
+<link rel="stylesheet" href="assets/css/style.css" />
+<?php } ?>
+  
+
 </head>
 
 <body>
+
+
   <!-- Navbar avec le bouton Parametres -->
   <header>
     <div class="col-12 m-0 p-0">
+    <?php if ($_COOKIE["theme"] == 1 || ! isset($_COOKIE["theme"])){ ?>
       <img src="assets/img/orangeimg.png" class="d-block align-items-left w-100">
+<?php } elseif ($_COOKIE["theme"] == 2 ){ ?>
+  <img src="assets/img/orangeimg.png" class="d-block align-items-left w-100">
+<?php } elseif ($_COOKIE["theme"] == 3 ){ ?>
+  <img src="assets/img/orangeimg.png" class="d-block align-items-left w-100">
+<?php } ?>
+      
     </div>
   </header>
   <div class="container-fluid d-flex justify-content-start mt-4">
     <div class="row">
+    <div class="col-1">
+    <?php if ($_COOKIE["theme"] == 1 || ! isset($_COOKIE["theme"])){ ?>
       <a class="navbar-brand" data-bs-toggle="modal" data-bs-target="#modalParameters">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#ff9100" class="bi bi-gear-fill" viewBox="0 0 16 16">
+          <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
+        </svg>
+      </a>
+<?php } elseif ($_COOKIE["theme"] == 2 ){ ?>
+  <a class="navbar-brand" data-bs-toggle="modal" data-bs-target="#modalParameters">
         <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white" class="bi bi-gear-fill" viewBox="0 0 16 16">
           <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
         </svg>
       </a>
+<?php } elseif ($_COOKIE["theme"] == 3 ){ ?>
+  <a class="navbar-brand" data-bs-toggle="modal" data-bs-target="#modalParameters">
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="white" class="bi bi-gear-fill" viewBox="0 0 16 16">
+          <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
+        </svg>
+      </a>
+<?php } ?>
+     </div>
+
     </div>
   </div>
-  <div class="col-12 mx-auto text-center">
-    <h1>Cactus ___</h1>
+  <div class="col-11 mx-auto text-center">
+    <h1  id="titleColor">Cactus ___</h1>
     <h2>Vos actus qui ne manquent pas de piquants</h2>
     <?php
     if (isset($_COOKIE["feed"])) {
       foreach ($feeds as $key => $value) {
         if ($_COOKIE["feed"] == $value) {
     ?>
-          <h3> Vous êtes dans la section <?= $key ?></h3>
+          <h3 class="mb-5"> Vous êtes dans la section <?= $key ?></h3>
       <?php
         }
       }
     } else {
       ?>
-      <h3> Vous êtes dans la section <?= array_key_first($feeds) ?></h3>
+      <h3 class="mb-5"> Vous êtes dans la section <?= array_key_first($feeds) ?></h3>
       <?php
     }?>
   </div>
@@ -110,7 +145,7 @@
 
   <div class="container-fluid">
 
-    <h3><?= array_key_first($feeds) ?></h3>
+
     <?php
                                   if (isset($_COOKIE["feed"])) {
                                     $testFeed = new BlogFeed($_COOKIE["feed"]);
@@ -130,7 +165,7 @@
                                                                                                   } ?>;"></i></div>
         <div class="col-8 col-lg-6 colArticle mt-4 p-4"><?= $testFeed->posts[$i]->title ?></div>
         <div class="col-lg-2 d-none mt-4 d-lg-block colArticle p-4" style="text-align: center"><?= $testFeed->posts[$i]->date ?></div>
-        <div class="col-1 colArticle mb-0 p-4">
+        <div class="col-1 colArticle mt-4 p-4">
           <button type="button" class="btn border-0 btn-light" data-bs-toggle="collapse" href="#collapseText<?= $i ?>" role="button" aria-expanded="false" aria-controls="collapseText<?= $i ?>" style="float: right;">
             <i class="fas fa-plus mx-1"></i>
           </button>
@@ -138,16 +173,16 @@
 
         <div class="col-10 mx-auto">
           <div class="collapse" id="collapseText<?= $i ?>">
-            <div class="row m-0 p-0">
+            <div class="row my-0 p-0 ">
               <div class="card card-body">
-                <div class="col-12">
+                <div class="col-6 mx-auto">
                   <img src="<?= $testFeed->posts[$i]->enclosure ?>" style="max-width : 100%">
                 </div>
-                <div class="col-12 p-4">
+                <div class="col-12 p-4 mx-auto">
                   <?= $testFeed->posts[$i]->summary ?>
                 </div>
-                <div class="col-12 p-4">
-                  <a type="button" href="<?= $testFeed->posts[$i]->link ?>">Aller à</a>
+                <div class="col-12 p-4 mx-auto  text-center">
+                  <a type="button" href="<?= $testFeed->posts[$i]->link ?>">Lire l'article sur le site ↗</a>
                 </div>
               </div>
             </div>
@@ -175,18 +210,18 @@
           </button>
         </div>
 
-        <div class="col-10 mt-0">
-          <div class="row">
-            <div class="collapse" id="collapseText<?= $nbPost ?>">
-              <div class="card card-body colArticle d-flex justify-content-centers">
+        <div class="col-10 mx-auto">
+          <div class="collapse" id="collapseText<?= $i ?>">
+            <div class="row my-0 p-0 ">
+              <div class="card card-body">
                 <div class="col-6 mx-auto">
-                  <img src="<?= $post->enclosure ?>" style="max-width : 100%">
+                  <img src="<?= $testFeed->posts[$i]->enclosure ?>" style="max-width : 100%">
                 </div>
                 <div class="col-12 p-4 mx-auto">
-                  <?= $post->summary ?>
+                  <?= $testFeed->posts[$i]->summary ?>
                 </div>
-                <div class="col-2 mx-auto p-4">
-                  <a type="button" href="<?= $post->link; ?>">Aller à</a>
+                <div class="col-12 p-4 mx-auto text-center">
+                  <a type="button" href="<?= $testFeed->posts[$i]->link ?>">Lire l'article sur le site ↗</a>
                 </div>
               </div>
             </div>
@@ -200,6 +235,8 @@
   ?>
 
   </div>
+
+  <footer class="mt-5 p-3 text-center"><p>© Nora Charef & Raphael Debraize | La Manu 2020 Le havre | Metions légales</p></footer>
 
 
 
