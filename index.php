a3e7b86bc36d8f83dca9d28bb6d930f7fbf91a82
Raphael Debraize <raphaeldebraize@gmail.com>
2021-02-11 10:06:48 +0100

    fin

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Cactus__</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
 
@@ -128,7 +128,7 @@
             <div>
               <h5 class="text-center">Nombre d'articles affichés</h5>
               <div class="text-center my-3">
-                <input type="radio" class="form-check-input ms-3" name="nbArticle" id="article5" value="5" <?php if ((isset($_COOKIE["nbArticle"]) && $_COOKIE["nbArticle"] == "5") || !isset($_COOKIE["nbArticle"])) { ?> checked <?php } ?>>
+                <input type="radio" class="form-check-input ms-3" name="nbArticle" id="article5" value="5" <?php if (!isset($_COOKIE["nbArticle"]) ||(isset($_COOKIE["nbArticle"]) && $_COOKIE["nbArticle"] == "5")) { ?> checked <?php } ?>>
                 <label for="article5" class="me-2">5 articles</label>
                 <input type="radio" class="form-check-input ms-3" name="nbArticle" id="article10" value="10" <?php if (isset($_COOKIE["nbArticle"]) && $_COOKIE["nbArticle"] == "10") { ?> checked <?php } ?>>
                 <label for="article10" class="me-2">10 articles</label>
